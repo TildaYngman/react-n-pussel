6162d161f7fe4d5d5ab71f06c615eaa566460c5e
Matilda Yngman <matilda.yngman@futurice.se>
2023-03-27 22:52:05 +0200

    Setting number of cols and rows to same as in the wireframe

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -2,4 +2,4 @@ import { BoardProps } from '../types/types';
 
 //Here you can change the number of rows and columns to adjust the number of tiles.
 
-export const config: BoardProps = { cols: 2, rows: 2 };
+export const config: BoardProps = { cols: 5, rows: 3 };
